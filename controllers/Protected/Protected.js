05833329ca76d93d451e08e7fe133bf2abf64829
xaruFushigi <xaru.fushigi@gmail.com>
2023-06-18 22:34:55 +0900

    removal of some files and folders

--- controllers/Protected/Protected.js
+++ controllers/Protected/Protected.js
@@ -1,16 +1,11 @@
-const { GoogleStrategy, express, expressSession, app,
-    pgSession, dotenv, pg, knex, db, pool, cors,
-    passport, passportLocal, localStrategy, bcrypt, jwt, 
-    crypto, cookieParser, csrf, csrfProtection} = require('../../dependencies');
-
 const Protected = (req, res, app) => {
-   
-    try { 
-        res.send('Protected route accessed successfully');
-    }
-    catch(error) { console.log(error) }
+  try {
+    res.send("Protected route accessed successfully");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-    Protected : Protected
-}
+  Protected: Protected,
+};
--- controllers/Root/root.js
+++ controllers/Root/root.js
@@ -1,12 +1,5 @@
-const { GoogleStrategy, express, expressSession, app,
-    pgSession, dotenv, pg, knex, db, pool, cors,
-    passport, passportLocal, localStrategy, bcrypt, jwt, 
-    crypto, cookieParser, csrf, csrfProtection} = require('../../dependencies');
-
-const RootLink = (req, res, db) => {
-    
-};
+const RootLink = (req, res) => {};
 
 module.exports = {
-    RootLink: RootLink
+  RootLink: RootLink,
 };
