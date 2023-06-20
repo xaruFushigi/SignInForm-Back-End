552d89023da288c5769fd90bf3ffe517b65f0cbc
xaruFushigi <xaru.fushigi@gmail.com>
2023-06-20 10:04:08 +0900

    Update signup.js

--- controllers/SignUp/signup.js
+++ controllers/SignUp/signup.js
@@ -11,27 +11,25 @@ const SignUpLink = async (req, res, db, bcrypt) => {
       return res.status(400).json({ error: "User already exists" });
     } //end of existingUser
 
-    if (!existingUser) {
-      // Hashing a password
-      const hashedPassword = await bcrypt.hash(password, 10);
-      // If the user DOES NOT already exist, insert a new user with the given name, email, and registration date into the 'users' table of the database:
-      const newUser = await db.transaction(async (trx) => {
-        await trx("users").insert({
-          name,
-          email,
-          joined: new Date(),
-        });
-        // Insert the hashed password into the 'login' table of the database along with the user's email:
-        await trx("login").insert({
-          email,
-          hash: hashedPassword,
-        });
+    // Hashing a password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // If the user DOES NOT already exist, insert a new user with the given name, email, and registration date into the 'users' table of the database:
+    const newUser = await db.transaction(async (trx) => {
+      await trx("users").insert({
+        name,
+        email,
+        joined: new Date(),
       });
-      // Send an HTTP response with status code 201, the newly created user object, and a success message:
-      res
-        .status(201)
-        .json({ user: newUser[0], message: "User created successfully" });
-    } // end of !existingUser
+      // Insert the hashed password into the 'login' table of the database along with the user's email:
+      await trx("login").insert({
+        email,
+        hash: hashedPassword,
+      });
+    });
+    // Send an HTTP response with status code 201, the newly created user object, and a success message:
+    return res
+      .status(201)
+      .json({ user: newUser[0], message: "User created successfully" });
   } catch (error) {
     // If there was an error during this process, log the error and send an HTTP response with status code 500 and an error message:
     console.log(error);
